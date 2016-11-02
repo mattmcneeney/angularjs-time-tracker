69d61e941eb1ec9d46486e4510f61ec708de40af
Matt McNeeney <matt@mattmc.co.uk>
2016-11-02 11:46:31 +0000

    Add time tracker directive

---
+++ time-tracker.js
@@ -0,0 +1,26 @@
+angular.module('yourApp', [])
+.directive('timeTracker', [ '$timeout', function($timeout) {
+   return {
+      restrict: 'A',
+      link: function(scope, element, attrs, ctrl) {
+
+         // The durations (in seconds) that we want to track
+         scope.durations = [ 0, 5, 10, 20, 30, 60, 120, 300 ];
+
+         // Setup the Google Analytics tracking calls
+         scope.trackDuration = function(secondsElapsed) {
+            ga('send', 'event', 'Time Tracker', 'Track', 'Seconds elapsed', secondsElapsed, { 'nonInteraction': 1 });
+         };
+
+         // Setup the tracking calls
+         scope.durations.forEach(function(duration) {
+            $timeout(function() {
+               scope.trackDuration(duration);
+            }, (duration * 1000));
+         });
+
+         // Log that time tracking is starting
+         console.log('Time tracker started');
+      }
+   };
+}]);
